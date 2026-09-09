fb35df225a738301744da5c453025ce53f913192
techmiguel <techmigue@gmail.com>
2026-09-08 21:24:59 -0400

    feat(inference): retrain command model with Freenove board dataset (98.2% accuracy)

--- firmware/src/models/vocabulario.h
+++ firmware/src/models/vocabulario.h
@@ -9,5 +9,5 @@ static const char* const VOCAB_CLASES[VOCAB_N_CLASES]    = {"hola_neo", "clima",
 static const char* const VOCAB_FRASES[VOCAB_N_CLASES]    = {"Hola NEO", "Clima", "Noticias", "El Toque", "Cripto", "Dado", "otras palabras o voces", "solo ruido ambiente"};
 static const uint16_t    VOCAB_OBJETIVOS[VOCAB_N_CLASES] = {170, 80, 80, 80, 80, 80, 120, 60};
 static const bool        VOCAB_ES_COMANDO[VOCAB_N_CLASES] = {true, true, true, true, true, true, false, false};
-static const float       VOCAB_UMBRALES[VOCAB_N_CLASES]  = {0.8500f, 0.8500f, 0.8500f, 0.8500f, 0.8500f, 0.8500f, 0.8500f, 0.8500f};
+static const float       VOCAB_UMBRALES[VOCAB_N_CLASES]  = {0.9835f, 0.9954f, 0.9823f, 0.9988f, 0.9979f, 0.9997f, 0.8500f, 0.8500f};
 static const int         VOCAB_COMANDO[VOCAB_N_CLASES]   = {1, 2, 3, 4, 5, 12, 0, 0};
